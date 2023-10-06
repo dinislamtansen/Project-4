be69ef96f97a13c8edc297d21a339daf5eb52bb8
tansen2425 <137645523+tansen2425@users.noreply.github.com>
2023-10-06 10:41:37 +0600

    Add files via upload

---
+++ no-1.js
@@ -0,0 +1,16 @@
+var a=5;
+var b=4;
+const sum=a+b;
+console.log(sum);
+
+let c= -6;
+let d= -9;
+
+const sum1= c+d;
+console.log(sum1);
+
+let e = 9;
+let f= 5;
+
+const sum2 = e+f;
+console.log(sum2);
